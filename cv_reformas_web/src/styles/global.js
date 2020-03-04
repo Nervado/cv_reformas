d36a7f2b2551b25c51a520319e7a8064f7fe14ad
Nervado <evandroabreudeoliveira@gmail.com>
2020-03-04 08:22:06 -0300

    fix: bug in import css at global stylest

--- cv_reformas_web/src/styles/global.js
+++ cv_reformas_web/src/styles/global.js
@@ -6,7 +6,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import 'react-perfect-scrollbar/dist/css/styles.css';
 
 export default createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
+
   * {
     margin: 0;
     padding: 0;
